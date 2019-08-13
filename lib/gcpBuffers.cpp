9fadfcb3712f9f4fe709ee9e470e074a7bc071fc
Robert Clapp <bob@sep.stanford.edu>
2019-08-13 19:04:37 +0000

    throw error when undefined

--- lib/gcpBuffers.cpp
+++ lib/gcpBuffers.cpp
@@ -158,38 +158,38 @@ void gcpBuffers::setName(const std::string &dir, const bool create) {
                      },
                      object_metadata->bucket(), object_metadata->name()));
     }
-
-    for (auto i = 0; i < _buffers.size(); i++) {
-      std::string hsh = std::to_string(
-          std::hash<std::string>{}(std::string("/buf") + std::to_string(i)));
-      //    _buffers[i]->setName(hsh.substr(0, 5) + std::string("buf") +
-      //    _buffers[i]->setName(std::string("buf") +
-      //                    std::to_string(i));
-      _buffers[i]->setName(_baseName + std::string("/") + hsh.substr(0, 5) +
-                           std::string("buf") + std::to_string(i));
-      //_buffers[i]->setName(_baseName
-      //+std::string("/")+ std::string("buf") + std::to_string(i));
-      std::shared_ptr<gcpBuffer> b =
-          std::dynamic_pointer_cast<gcpBuffer>(_buffers[i]);
-      b->setBucketName(_bucket);
-    }
-    if (create)
-      for (auto &n : changes) n.get();
   }
-  void gcpBuffers::createBuffers(const bufferState state) {
-    std::cerr << "creating buffers" << std::endl;
-    std::vector<int> ns = _hyper->getNs();
-    blockParams b = _blocking->makeBlocks(ns);
-    namespace gcs = google::cloud::storage;
-    google::cloud::v0::StatusOr<gcs::Client> client =
-        gcs::Client::CreateDefaultClient();
-    if (!client) throw SEPException("client is dead on createBuffers");
-    ;
-    for (int i = 0; i < b._ns.size(); i++) {
-      _buffers.push_back(std::make_shared<gcpBuffer>(_name, b._ns[i], b._fs[i],
-                                                     _compress, state));
-    }
-
-    _n123blocking = b._nblocking;
-    _axisBlocking = b._axesBlock;
+  for (auto i = 0; i < _buffers.size(); i++) {
+    std::string hsh = std::to_string(
+        std::hash<std::string>{}(std::string("/buf") + std::to_string(i)));
+    //    _buffers[i]->setName(hsh.substr(0, 5) + std::string("buf") +
+    //    _buffers[i]->setName(std::string("buf") +
+    //                    std::to_string(i));
+    _buffers[i]->setName(_baseName + std::string("/") + hsh.substr(0, 5) +
+                         std::string("buf") + std::to_string(i));
+    //_buffers[i]->setName(_baseName
+    //+std::string("/")+ std::string("buf") + std::to_string(i));
+    std::shared_ptr<gcpBuffer> b =
+        std::dynamic_pointer_cast<gcpBuffer>(_buffers[i]);
+    b->setBucketName(_bucket);
   }
+  if (create)
+    for (auto &n : changes) n.get();
+}
+void gcpBuffers::createBuffers(const bufferState state) {
+  std::cerr << "creating buffers" << std::endl;
+  std::vector<int> ns = _hyper->getNs();
+  blockParams b = _blocking->makeBlocks(ns);
+  namespace gcs = google::cloud::storage;
+  google::cloud::v0::StatusOr<gcs::Client> client =
+      gcs::Client::CreateDefaultClient();
+  if (!client) throw SEPException("client is dead on createBuffers");
+  ;
+  for (int i = 0; i < b._ns.size(); i++) {
+    _buffers.push_back(std::make_shared<gcpBuffer>(_name, b._ns[i], b._fs[i],
+                                                   _compress, state));
+  }
+
+  _n123blocking = b._nblocking;
+  _axisBlocking = b._axesBlock;
+}
