c047a5367fa32924cae91785b0435b17634b293a
Bob Clapp <bob@sep.stanford.edu>
2019-10-29 18:10:32 +0000

    debug

--- lib/buffers.cpp
+++ lib/buffers.cpp
@@ -568,7 +568,7 @@ void buffers::putWindow(const std::vector<int> &nw, const std ::vector<int> &fw,
   long long change = 0;
   for (auto i = 0; i < pwind.size(); i++) {
     std::vector<int> n_w(7), f_w(7), j_w(7), nG(7), fG(7), blockG(7);
-
+    std::cerr << "what is fgoinf on " << i << " " << pwind[i] << std::endl;
     size_t pos =
         _buffers[pwind[i]]->localWindow(n, f, j, n_w, f_w, j_w, nG, fG, blockG);
 
